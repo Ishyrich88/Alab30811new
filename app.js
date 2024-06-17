8887ba34957bf1e8f658fd26869d747759bf61ad
Ishyrich88 <167790636+Ishyrich88@users.noreply.github.com>
2024-06-17 10:51:09 -0400

    checking that there is 2 odd numbers in the set of 4

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 // Part one 
 
-const n1 = 11
-const n2 = 13
-const n3 = 12
-const n4 = 14
+let n1 = 11
+let n2 = 13
+let n3 = 12
+let n4 = 14
 
 // here I console log the resul to make sure it equals to 50  
 
 console.log( n1+n2+n3+n4)
+
+// Here I created a function to check if there's 2 odd numbers
+
+function twoOddNumbers(n1, n2, n3, n4) {
+    const numbers = [n1, n2, n3, n4];
+    const oddCount = numbers.filter(num => num % 2 !== 0).length;
+    return oddCount >= 2;
+}
+
+console.log(twoOddNumbers(n1, n2, n3, n4));
